f37e6f086f3412d43d561a9693d3e8eb29d96a89
ralph <ralph-joseph.tatt@infinityworks.com>
2019-09-27 09:48:51 +0100

    Updating from file mangement to database mangement

--- src/classes.py
+++ src/classes.py
@@ -1,24 +1,19 @@
-import time
-
-
 class Person:
     # name
-    def __init__(self, first_name, surname, preference=None, team=None):
-        self.name = first_name
-        self.full_name = first_name + " " + surname
-        self.surname = surname
+    def __init__(self, person_id, first_name, second_name, team, preference):
+        self.person_id = person_id
+        self.first_name = first_name
+        self.second_name = second_name
+        self.full_name = first_name + " " + second_name
+        self.second_name = second_name
         self.preference = preference
         self.team = team
-        self.rounds_made = 0
 
     def __str__(self):
         return f"{self.full_name} ({self.team})"
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
-
-    def made_round(self):
-        self.rounds_made += 1
 
     def get_name(self):
         return self.full_name
@@ -33,24 +28,25 @@ class Person:
         return self.rounds_made
 
     def get_table_headers(self):
-        return ["Name", "Team Name", "Fav. Drink", "Rounds Made"]
+        return ["Name", "Team Name", "Fav. Drink"]
 
     def get_details(self):
-        return [self.get_name(), self.get_team(), self.get_preference(), self.get_rounds_made()]
+        return [self.get_name(), self.get_team(), self.get_preference()]
 
     def set_preference(self, preference):
         self.preference = preference
 
 
 class Drink:
-    def __init__(self, name):
+    def __init__(self, drink_id, name):
+        self.drink_id = drink_id
         self.name = name
 
     def __str__(self):
         return self.name
 
     def __eq__(self, other):
-        return self.__dict__ == other.__dict__
+        return self.drink_id == other.drink_id
 
     def get_table_headers(self):
         return ["Name"]
@@ -60,9 +56,16 @@ class Drink:
 
 
 class Team:
-    def __init__(self, name, location):
+    def __init__(self, team_id, name, location):
+        self.team_id = team_id
         self.name = name
         self.location = location
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.team_id == other.team_id
 
     def get_table_headers(self):
         return ["Name", "Location"]
@@ -70,22 +73,52 @@ class Team:
     def get_details(self):
         return [self.name, self.location]
 
+
+# A teams order is added to a round
+class Round:
+    def __init__(self, round_id, maker, active, team):
+        self.round_id = round_id
+        self.maker = maker
+        self.orders = {}
+        self.active = active
+        self.team = team
+
     def __str__(self):
-        return self.name
+        return f"Team:{self.team} - Maker:{self.maker.get_name()}"
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
+    def get_maker(self):
+        return self.maker
+
+    def add_order(self, order):
+        self.orders[order.order_id] = order
+
+    def get_team(self):
+        return self.team
+
+    def finish_round(self):
+        self.active = False
+
+    def get_table_headers(self):
+        return ["Team Name", "Maker", "Active"]
+
+    def get_details(self):
+        return [self.get_team(), self.get_maker(), self.active]
 
 # User makes an order for a drink
 class Order:
-    def __init__(self, person, drink, notes=None):
+    def __init__(self, order_id, person, drink, round_id, notes=None):
+        self.order_id = order_id
         self.person = person
         self.drink = drink
         self.notes = notes
+        self.round_id = round_id
 
-    def change_order(self, new_drink):
-        self.drink = new_drink
+    # def change_order(self, new_drink):
+    #     self.drink = new_drink
+    #
 
     def get_table_headers(self):
         return ["Person", "Drink", "Notes"]
@@ -98,58 +131,3 @@ class Order:
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
-
-
-# A teams order is added to a round
-class Round:
-    def __init__(self, maker, team="General", length=600):
-        self.maker = maker
-        self.orders = []
-        self.active = True
-        self.team = team
-        self.start_time = time.time()
-        self.length = length
-
-    def __str__(self):
-        return f"Team:{self.team} - Maker:{self.maker.get_name()}"
-
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
-
-    def get_maker(self):
-        return self.maker
-
-    def add_order(self, order):
-        self.orders.append(order)
-
-    def get_team(self):
-        return self.team
-
-    def elapsed_time(self):
-        duration = time.time() - self.start_time
-        if duration > 60:
-            duration = duration / 60
-            return str(round(duration)) + "m"
-        return str(round(duration)) + "s"
-
-    def finish_round(self):
-        self.maker.rounds_made += 1
-        self.duration = self.elapsed_time()
-        self.active = False
-
-    def get_order_size(self):
-        return len(self.orders)
-
-    def get_table_headers(self):
-        if self.active:
-            time_string = "Elasped Time"
-        else:
-            time_string = "Duration"
-        return ["Team Name", "Maker", time_string, "Order Size"]
-
-    def get_details(self):
-        if self.active:
-            time_type = self.elapsed_time()
-        else:
-            time_type = self.duration
-        return [self.get_team(), self.get_maker(), time_type, self.get_order_size()]
